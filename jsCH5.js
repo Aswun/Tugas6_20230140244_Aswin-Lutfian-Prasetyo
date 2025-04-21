f3350227e14239f4bd61c3ac6db977ac5925d2a0
Aswun <aswinlutfian32@gmail.com>
2025-04-21 16:19:53 +0700

    up

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video");
+});
+
+audio.addEventListener("click", function video() {
+    alert("ini audio");
+});
+
+gambar.addEventListener("click", function video() {
+    alert("ini gambar");
+});
